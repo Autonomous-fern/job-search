9130e4035dff931052ab754f3bc0aaefde6fb671
autonomous fern <autonomous.fern@gmail.com>
2022-08-16 12:56:37 +0200

    airtable api first usage

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import os
-import env
+import creds
+from pyairtable import Table
 
+# !! AIRTABLE API IS LIMITED TO 5 requests/sec !!
 
-print(env.AIRTABLE_API_KEY)
+api_key = creds.AIRTABLE_API_KEY
+base_id = creds.BASE_ID
+table_id = creds.TABLE_ID
+
+table = Table(api_key, base_id, table_id)
