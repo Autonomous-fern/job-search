cc3aefb627c727834752974a18cfa8a17f34c474
autonomous fern <autonomous.fern@gmail.com>
2022-08-31 21:59:17 +0200

    successfully got jobtitle, company & location

--- find_offer.py
+++ find_offer.py
@@ -9,3 +9,19 @@ driver = webdriver.Chrome(PATH)  # select chrome driver
 
 url1 = "https://www.seek.com.au/devops-jobs/in-All-Sydney-NSW?salaryrange=0-80000&salarytype=annual"
 page = driver.get(url1)
+time.sleep(3)
+
+announce_titles_elements = driver.find_elements(By.XPATH, "//a[@data-automation= 'jobTitle']")
+announce_titles = [element.text for element in announce_titles_elements]
+
+announce_company_elements = driver.find_elements(By.XPATH, "//a[@data-automation= 'jobCompany']")
+announce_companies = [element.text for element in announce_company_elements]
+
+announce_location_elements = driver.find_elements(By.XPATH, "//a[@data-automation= 'jobLocation']")
+announce_locations = [element.text for element in announce_location_elements]
+
+
+print(announce_locations)
+
+
+# need to separate functions : open chrome, find job titles on the open chrome window
